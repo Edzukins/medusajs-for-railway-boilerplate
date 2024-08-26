3d64695a4c7220edf55514ba1d0ba2cc670105ef
Edgars <edgars.skruodis@gmail.com>
2024-08-26 16:21:24 +0300

    *)push test

--- medusajs-storefront/src/app/[countryCode]/(main)/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -8,9 +8,9 @@ import { ProductCollectionWithPreviews } from "types/global"
 import { cache } from "react"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Robotiem.lv",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Everything you need to build your own robot. From parts to kits, we have it all.",
 }
 
 const getCollectionsWithProducts = cache(
